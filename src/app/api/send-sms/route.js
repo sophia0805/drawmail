5f21c08c552ba9b43491cbfa35aaceddc89bb764
sophia <sophiawang8509@gmail.com>
2025-08-10 18:14:22 -0400

    switching apis cuz i can't send msgs to nonverified

--- src/app/api/send-sms/route.js
+++ src/app/api/send-sms/route.js
@@ -1,7 +1,7 @@
 import telnyx from 'telnyx';
 import FormData from 'form-data';
 
-const client = telnyx(process.env.TELYNXKEY);
+const client = telnyx("KEY019895E9C363EDB791691EB5A851CBB6_WcjhgRlw4BBRbSpbqt4gPM");
 
 export async function POST(request) {
   try {
@@ -27,28 +27,19 @@ export async function POST(request) {
             console.error('ImgBB upload error:', error);
         }
     }
-/*
     const messageData = {
-      from: process.env.TELYNXNUMBER,
-      messaging_profile_id: process.env.PROFILEID,
-      to: phoneNumber,
-      text: smsMessage
+        from: "+14704028171",
+        messaging_profile_id: "40019895-65b5-45ea-8c44-2e0795f8de09",
+        to: phoneNumber,
+        text: message || 'Check out my drawing!',
+        media_urls: [mediaUrl],
+        type: 'MMS'
     };
-    
-    // Only add MMS fields if we have a media URL
-    if (mediaUrl) {
-      messageData.type = 'MMS';
-      messageData.media_urls = [mediaUrl];
-    } else {
-      messageData.type = 'SMS';
-    }
-
     const messageResponse = await client.messages.create(messageData);
     console.log('Telnyx response:', messageResponse);
-    */
     return Response.json({ 
       success: true, 
-      message: 'SMS sent successfully',
+      message: 'MMS sent successfully',
       messageId: messageResponse.data.id,
       mediaUrl: mediaUrl
     });
